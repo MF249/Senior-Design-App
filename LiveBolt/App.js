0eb2c893a060ac3329ae1f5be5f5eb2b935d4db3
MF249 <mfaizel@knights.ucf.edu>
2022-08-17 18:51:50 -0400

    added react-navigation and 1st register screen

--- LiveBolt/App.js
+++ LiveBolt/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text, ScrollView, TextInput, StyleSheet, Pressable } from 'react-native';
 
-const App = () => {
+const LoginScreen = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View>
@@ -37,10 +39,96 @@ const App = () => {
       <Pressable style={{ paddingTop: 20 }}>
         <Text style={{color: 'white'}}>Forgot your password?</Text>
       </Pressable>
-      <Pressable style={{ position:'absolute', bottom: 0, paddingBottom: 20 }}>
+      <Pressable style={{ position:'absolute', bottom: 0, paddingBottom: 50 }} onPress={() => navigation.navigate('Account Creation')}>
         <Text style={{color: 'white'}}>Don't have an account? Sign up here.</Text>
       </Pressable>
     </ScrollView>
+  );
+};
+
+const RegisterScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={stylesTwo.container}>
+      <View style={stylesTwo.firstView}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            backgroundColor: 'white',
+            color: 'gray',
+            borderRadius: 15,
+          }}
+          defaultValue="   First Name"
+        />
+      </View>
+      <View style={stylesTwo.inputView}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            backgroundColor: 'white',
+            color: 'gray',
+            borderRadius: 15,
+          }}
+          defaultValue="   Last Name"
+        />
+      </View>
+      <View style={stylesTwo.inputView}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            backgroundColor: 'white',
+            color: 'gray',
+            borderRadius: 15,
+          }}
+          defaultValue="   Email Address"
+        />
+      </View>
+      <View style={stylesTwo.lastView}>
+        <TextInput
+          style={{
+            height: 40,
+            width: 300,
+            backgroundColor: 'white',
+            color: 'gray',
+            borderRadius: 15,
+          }}
+          defaultValue="   Phone Number"
+        />
+      </View>
+      <Pressable style={stylesTwo.continueButton}>
+        <Text style={{color: 'white'}}>Continue</Text>
+      </Pressable>
+    </ScrollView>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen  
+          name="Login" 
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        
+        <Stack.Screen 
+          name="Account Creation" 
+          component={RegisterScreen} 
+          options={{
+            headerStyle: {
+              backgroundColor: '#144C81',
+            },
+            headerTitleAlign: 'center',
+            headerTintColor: '#FFF',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -60,6 +148,34 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   loginButton: {
+    backgroundColor: '#041847',
+    borderRadius: 10,
+    height: 40,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
+const stylesTwo = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    backgroundColor: '#D7D7D7',
+  },
+  inputView: {
+    paddingVertical: 10,
+  },
+  firstView: {
+    paddingVertical: 10,
+    paddingTop: 100,
+  },
+  lastView: {
+    paddingVertical: 10,
+    paddingBottom: 100,
+  },
+  continueButton: {
     backgroundColor: '#041847',
     borderRadius: 10,
     height: 40,
