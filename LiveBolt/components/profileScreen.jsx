03cf4b1e0d37b20b291c2f1027d30cdd7d68cad9
Joel Shaw <joelshaw@knights.ucf.edu>
2022-10-16 17:43:20 -0400

    Profile updates
    Added fetch fuctionality

--- LiveBolt/components/profileScreen.jsx
+++ LiveBolt/components/profileScreen.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Text, ScrollView, StyleSheet, Pressable, Image } from 'react-native';
 import info from '../images/icons8-info-48.png';
 
 function ProfileScreen({navigation}) {
+    const [name, setName] = useState([]);
+    const [phone, setPhone] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [username, setUsername] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    useEffect(() => {
+        fetch("https://livebolt-rest-api.herokuapp.com/api/profileUser", {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId: "633cd5813d6503bedad92a50"
+            })
+        })
+        .then((response) => response.json()).then((responseJson) => {
+            console.log(responseJson);
+        })
+    }, [])
+
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Image style={styles.imageFormat} source={info}/>
-            <Text>John Smith</Text>
-            <Text>john_smith</Text>
-            <Text>johnsmith@email.com</Text>
+            <Text>{name}</Text>
+            <Text>{phone}</Text>
+            <Text>{email}</Text>
+            <Text>{username}</Text>
+            <Text>{password}</Text>
         </ScrollView>
     );
 };
